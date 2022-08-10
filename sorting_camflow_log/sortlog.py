e60461870e454d59ec7ae9fceb48b673eb8e995d
Muhammad Shahpar Nafees Khan <66970201+shahparkhan@users.noreply.github.com>
2022-08-10 13:52:14 +0500

    Updated sortlog.py code

--- sorting_camflow_log/sortlog.py
+++ sorting_camflow_log/sortlog.py
@@ -1,8 +1,26 @@
-from json import loads, dumps, dump
-from os import write
+from json import loads
 from queue import PriorityQueue
+import sys
+import os
 
-pq = PriorityQueue()
+'''
+ --------------------------------------------------------------------------------
+ @What it does?
+    The following Python module is designed to ingest a CamFlow log and sort it
+    in a way that all vertices are piled up at the top followed by all the edges
+    in a sequenctial order based on their 'relation_id'.
+ 
+ @When should you use it?
+    If there is a strict need in a module that requires a vertex to be present
+    before the edge of that vertex is encountered in a log then this Python 
+    module can help you bring your CamFlow log in a desired shape. 
+ 
+ @authors Shahpar Khan, Mashal Abbas
+ --------------------------------------------------------------------------------
+'''
+
+
+# Function that returns True if the current JSON object is a vertex --- False if it is an Edge
 def isVertex(obj):
 
     if (obj["type"] == "Entity") or (obj["type"] == "Activity"):
@@ -10,6 +28,8 @@ def isVertex(obj):
 
     return False
 
+
+# Function that extract relation_ids from the JSON object
 def extractRelationID(obj):
 
     try:
@@ -19,34 +39,44 @@ def extractRelationID(obj):
 
     return None
 
-# function to read log at a given path
-def readLog(path):
-    # return open(path, 'r').readlines()
-    output_file = open("./sorted_social_network_socfb_2.log", "a")
-    myfile = open(path, 'r')
-    print("reading first line")
-    myline = myfile.readline()
-    # counter = 0
-    while myline:
+
+# Function to dump all the edges in priority queue based on the relation_ids
+def dumpPriorityQueue(pq, output_file):
+
+    while not pq.empty():
+        (_, line) = pq.get()
+        output_file.write(line)
+
+
+# Function to read log at a given path 
+def readWriteLog(input_log_path, output_log_name):
+
+    # Opening files
+    output_file = open(output_log_name, "a")
+    input_file = open(input_log_path, 'r')
+    
+    # Initializing priority queue
+    pq = PriorityQueue()
+
+    line = input_file.readline()
+    while line:
         try:
-            obj = loads(myline)
+            obj = loads(line)
             if isVertex(obj): 
-                # dump(obj, output_file)
-                output_file.write(myline)
+                output_file.write(line)
             else:
                 relation_id = extractRelationID(obj)
-                pq.put((relation_id, myline))
+                pq.put((relation_id, line))
         except:
-            print("BLEKH") 
-            print(myline)  
+            print("Error in ingesting the following line:") 
+            print(line)  
 
-        myline = myfile.readline()
-        # counter = counter + 1
-        # if counter == 4:
-        #     break
+        line = input_file.readline()
+
+    dumpPriorityQueue(pq, output_file)
+
     output_file.close()
-    myfile.close()
-
+    input_file.close()
 
 
 def checkFromAnnot(obj):
@@ -59,51 +89,25 @@ def checkFromAnnot(obj):
     return False
 
 
-
-def main(logpath):
-    # reading log
-    # lines = readLog(logpath)
-    # print("Done Reading")
-
-    # # print(lines[0])
-    # # vertices = []
-    # edges = PriorityQueue()
-
-    # # typesss = []
-    # print("here123")
+def main(input_log_path, output_log_name):
+    
+    readWriteLog(input_log_path, output_log_name)
+    print("Done...")
 
 
-    # print("Going into writing onto the file")
-
-    # for i in range(0, 3):
-    #     print("iteration number:", i)
-    #     obj = loads(lines[i])
-    #     print("\nloaded")
-    #     dumped = dumps(obj)
-    #     print("\ndumped")
-    #     output_file.write(dumped)
-    #     output_file.write('\n')
-    #     print("\nwritten")
-
-    readLog(logpath)
-    print("file reading done")
-
-    output_file = open("./sorted_social_network_socfb_2.log", "a")
-
-    print("extracting pq starting")
-
-    while not pq.empty():
-        (_, line) = pq.get()
-        output_file.write(line)
-    output_file.close()
-
-    print("extracting pq stopped")
-
-
-
-
-
-
-
-
-main("./social_network_socfb_2.log")
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) != 3:
+            raise Exception("run python3 sortlog.py <input_log_path> <output_log_name>")
+        else:
+            print("Starting...")
+            print("Input log path:", sys.argv[1])
+            print("Output log name:", sys.argv[2])
+            main(sys.argv[1], sys.argv[2])
+        
+    except KeyboardInterrupt:
+        print("Exiting...")
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
